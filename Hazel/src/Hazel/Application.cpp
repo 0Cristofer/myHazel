5dbca377566c0822302bda50503da30e2c26f692
Cristofer Oswald <cristoferoswald@gmail.com>
2024-02-28 19:40:09 -0500

    Reset project to use premake instead of cmake.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,75 +1,19 @@
-//
-// Created by crist on 16/01/2021.
-//
+#include "Application.h"
 
-#include <glad/glad.h>
+namespace Hazel {
 
-#include "Application.hpp"
+	Application::Application()
+	{
+	}
 
-namespace Hazel
-{
-    Application* Application::s_Instance = nullptr;
 
-    Application::Application()
-    {
-        HZ_CORE_ASSERT(!s_Instance, "Application already exists")
-        s_Instance = this;
+	Application::~Application()
+	{
+	}
 
-        m_Window = std::unique_ptr<Window>(Window::Create());
-        m_Window->SetEventCallback([this] (Event& event) { this->OnEvent(event); });
+	void Application::Run()
+	{
+		while (true);
+	}
 
-        m_ImGuiLayer = new ImGuiLayer();
-        PushOverlay(m_ImGuiLayer);
-    }
-
-    void Application::Run()
-    {
-        while (m_Running)
-        {
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            for (Layer* layer : m_LayerStack)
-                layer->OnUpdate();
-
-            m_ImGuiLayer->Begin();
-            for (Layer* layer : m_LayerStack)
-                layer->OnImGuiRender();
-            m_ImGuiLayer->End();
-
-            m_Window->OnUpdate();
-        }
-    }
-
-    void Application::OnEvent(Event& event)
-    {
-        EventDispatcher dispatcher(event);
-
-        dispatcher.Dispatch<WindowCloseEvent>(
-                [this] (WindowCloseEvent& event) -> bool { return this->OnWindowClose(event); });
-
-        for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
-        {
-            (*--it)->OnEvent(event);
-            if (event.Handled)
-                break;
-        }
-    }
-
-    bool Application::OnWindowClose(WindowCloseEvent& event)
-    {
-        m_Running = false;
-        return true;
-    }
-
-    void Application::PushLayer(Layer *layer)
-    {
-        m_LayerStack.PushLayer(layer);
-    }
-
-    void Application::PushOverlay(Layer *overlay)
-    {
-        m_LayerStack.PushOverlay(overlay);
-    }
-
-    Application::~Application() = default;
 }
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,48 +1,21 @@
-//
-// Created by crist on 16/01/2021.
-//
-
-#include <Hazel.hpp>
-#include <imgui.h>
-
-class ExampleLayer : public Hazel::Layer
-{
-public:
-    ExampleLayer() : Layer("Example") {}
-
-    void OnUpdate() override
-    {
-    }
-
-    void OnImGuiRender() override
-    {
-        ImGui::Begin("TESTE APP");
-
-        ImGui::Text("Teste texto");
-
-        ImGui::End();
-    }
-
-    void OnEvent(Hazel::Event &event) override
-    {
-    }
-};
+#include <Hazel.h>
 
 class Sandbox : public Hazel::Application
 {
 public:
-    Sandbox()
-    {
-        PushLayer(new ExampleLayer());
-    }
+	Sandbox()
+	{
 
-    ~Sandbox() override
-    {
+	}
 
-    }
+	~Sandbox()
+	{
+
+	}
+
 };
 
 Hazel::Application* Hazel::CreateApplication()
 {
-    return new Sandbox();
+	return new Sandbox();
 }
