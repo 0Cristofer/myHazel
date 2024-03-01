6002c92c0dba10a175c554ad134f01fcc9e05ac5
Cristofer Oswald <cristoferoswald@gmail.com>
2024-02-29 23:14:16 -0500

    Created event system

---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Hazel
+{
+	class HAZEL_API KeyEvent : public Event
+	{
+	public:
+		int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryApplication | EventCategoryInput)
+
+	protected:
+		explicit KeyEvent(const int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(const int keycode, const int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		int GetRepeatCount () const { return m_RepeatCount; }
+
+		[[nodiscard]] std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+
+	private:
+		int m_RepeatCount;
+	};
+
+	class KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		explicit KeyReleasedEvent(const int keycode)
+			: KeyEvent(keycode) {}
+
+
+		[[nodiscard]] std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+}
