0dc77970f26a90583caad951ee113391a8670177
Cristofer Oswald <cristoferoswald@gmail.com>
2024-03-02 16:40:30 -0500

    Added Glad

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,7 +3,7 @@
 
 #include "Window.h"
 
-#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 namespace Hazel {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
--- Hazel/src/Hazel/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Hazel/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
 
+#include <glad/glad.h>
+
 namespace Hazel
 {
 	static bool s_GLFWInitialized = false;
@@ -50,6 +52,8 @@ namespace Hazel
 		                            nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
 		SetVSync(true);
 
 		// Set GLFW callbacks
